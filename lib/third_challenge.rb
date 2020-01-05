615b16fb9f99c2da4eaec9382848af9742e70030
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 05:15:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,16 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "Lord Mantague", :age  => "53"
-        },
-      :matriarch => {
-        :name => "Lady Mantague", :age  => "54"
-        },
-      :hero => {
-        :name => "Romeo", :age  => "15", :status =>"alive"
-        },
+      :patriarch => {:name => "Lord Mantague", :age  => "53"},
+      :matriarch => {:name => "Lady Mantague", :age  => "54"},
+      :hero => {:name => "Romeo", :age  => "15", :status =>"alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        :name =>"Lord Capulet", :age  => "50"
-        },
-      :matriarch => {
-        :name =>"Lady Capulet", :age  => "51"
-        },
-      :heroine => {
-        :name =>"Juliet", :age  => "15", :status =>"Alive"
-        },
+      :patriarch => {:name =>"Lord Capulet", :age  => "50"},
+      :matriarch => {:name =>"Lady Capulet", :age  => "51"},
+      :heroine => {:name =>"Juliet", :age  => "15", :status =>"alive"},
       :heroine_friends => []
    }
   }
-
-  
 end
